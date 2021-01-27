9eef7dcd1223d1125d31574e6dfe5d408f3a752b
PaulZC <pc235603@gmail.com>
2021-01-27 10:42:00 +0000

    Move #define 's into .h. Move eeprom_coeff and psensor_resolution_osr into the class

--- src/SparkFun_PHT_MS8607_Arduino_Library.h
+++ src/SparkFun_PHT_MS8607_Arduino_Library.h
@@ -57,6 +57,104 @@
 #define Serial SerialUSB
 #endif
 
+// HSENSOR device address
+const uint8_t MS8607_HSENSOR_ADDR = 0x40; // 0b1000000
+
+// HSENSOR device commands
+#define HSENSOR_RESET_COMMAND 0xFE
+#define HSENSOR_READ_HUMIDITY_W_HOLD_COMMAND 0xE5
+#define HSENSOR_READ_HUMIDITY_WO_HOLD_COMMAND 0xF5
+#define HSENSOR_READ_SERIAL_FIRST_8BYTES_COMMAND 0xFA0F
+#define HSENSOR_READ_SERIAL_LAST_6BYTES_COMMAND 0xFCC9
+#define HSENSOR_WRITE_USER_REG_COMMAND 0xE6
+#define HSENSOR_READ_USER_REG_COMMAND 0xE7
+
+// Processing constants
+#define HSENSOR_TEMPERATURE_COEFFICIENT (float)(-0.15)
+#define HSENSOR_CONSTANT_A (float)(8.1332)
+#define HSENSOR_CONSTANT_B (float)(1762.39)
+#define HSENSOR_CONSTANT_C (float)(235.66)
+
+// Coefficients for temperature computation
+#define TEMPERATURE_COEFF_MUL (175.72)
+#define TEMPERATURE_COEFF_ADD (-46.85)
+
+// Coefficients for relative humidity computation
+#define HUMIDITY_COEFF_MUL (125)
+#define HUMIDITY_COEFF_ADD (-6)
+
+// Conversion timings
+#define HSENSOR_CONVERSION_TIME_12b 16
+#define HSENSOR_CONVERSION_TIME_10b 5
+#define HSENSOR_CONVERSION_TIME_8b 3
+#define HSENSOR_CONVERSION_TIME_11b 9
+
+#define HSENSOR_RESET_TIME 15 // ms value
+
+// HSENSOR User Register masks and bit position
+#define HSENSOR_USER_REG_RESOLUTION_MASK 0x81
+#define HSENSOR_USER_REG_END_OF_BATTERY_MASK 0x40
+#define HSENSOR_USER_REG_ENABLE_ONCHIP_HEATER_MASK 0x4
+#define HSENSOR_USER_REG_DISABLE_OTP_RELOAD_MASK 0x2
+#define HSENSOR_USER_REG_RESERVED_MASK                                         \
+  (~(HSENSOR_USER_REG_RESOLUTION_MASK | HSENSOR_USER_REG_END_OF_BATTERY_MASK | \
+     HSENSOR_USER_REG_ENABLE_ONCHIP_HEATER_MASK |                              \
+     HSENSOR_USER_REG_DISABLE_OTP_RELOAD_MASK))
+
+// HTU User Register values
+// Resolution
+#define HSENSOR_USER_REG_RESOLUTION_12b 0x00
+#define HSENSOR_USER_REG_RESOLUTION_11b 0x81
+#define HSENSOR_USER_REG_RESOLUTION_10b 0x80
+#define HSENSOR_USER_REG_RESOLUTION_8b 0x01
+
+// End of battery status
+#define HSENSOR_USER_REG_END_OF_BATTERY_VDD_ABOVE_2_25V 0x00
+#define HSENSOR_USER_REG_END_OF_BATTERY_VDD_BELOW_2_25V 0x40
+// Enable on chip heater
+#define HSENSOR_USER_REG_ONCHIP_HEATER_ENABLE 0x04
+#define HSENSOR_USER_REG_OTP_RELOAD_DISABLE 0x02
+
+// PSENSOR device address
+const uint8_t MS8607_PSENSOR_ADDR = 0x76; // 0b1110110
+
+// PSENSOR device commands
+#define PSENSOR_RESET_COMMAND 0x1E
+#define PSENSOR_START_PRESSURE_ADC_CONVERSION 0x40
+#define PSENSOR_START_TEMPERATURE_ADC_CONVERSION 0x50
+#define PSENSOR_READ_ADC 0x00
+
+#define PSENSOR_CONVERSION_OSR_MASK 0x0F
+
+#define PSENSOR_CONVERSION_TIME_OSR_256 1
+#define PSENSOR_CONVERSION_TIME_OSR_512 2
+#define PSENSOR_CONVERSION_TIME_OSR_1024 3
+#define PSENSOR_CONVERSION_TIME_OSR_2048 5
+#define PSENSOR_CONVERSION_TIME_OSR_4096 9
+#define PSENSOR_CONVERSION_TIME_OSR_8192 18
+
+// PSENSOR commands
+#define PROM_ADDRESS_READ_ADDRESS_0 0xA0
+#define PROM_ADDRESS_READ_ADDRESS_1 0xA2
+#define PROM_ADDRESS_READ_ADDRESS_2 0xA4
+#define PROM_ADDRESS_READ_ADDRESS_3 0xA6
+#define PROM_ADDRESS_READ_ADDRESS_4 0xA8
+#define PROM_ADDRESS_READ_ADDRESS_5 0xAA
+#define PROM_ADDRESS_READ_ADDRESS_6 0xAC
+#define PROM_ADDRESS_READ_ADDRESS_7 0xAE
+
+// Coefficients indexes for temperature and pressure computation
+#define CRC_INDEX 0
+#define PRESSURE_SENSITIVITY_INDEX 1
+#define PRESSURE_OFFSET_INDEX 2
+#define TEMP_COEFF_OF_PRESSURE_SENSITIVITY_INDEX 3
+#define TEMP_COEFF_OF_PRESSURE_OFFSET_INDEX 4
+#define REFERENCE_TEMPERATURE_INDEX 5
+#define TEMP_COEFF_OF_TEMPERATURE_INDEX 6
+#define COEFFICIENT_NUMBERS 7
+
+#define MAX_CONVERSION_TIME HSENSOR_CONVERSION_TIME_12b
+
 enum MS8607_humidity_i2c_master_mode
 {
        MS8607_i2c_hold,
@@ -278,6 +376,12 @@ public:
        float getHumidity();    //Returns the latest humidity measurement
        double adjustToSeaLevel(double absolutePressure, double actualAltitude);
        double altitudeChange(double currentPressure, double baselinePressure);
+
+
+   // Storage for the 'global' parameters
+   uint16_t eeprom_coeff[COEFFICIENT_NUMBERS + 1]; //Pressure sensor eeprom coefficients
+   enum MS8607_pressure_resolution psensor_resolution_osr;
+
 
 private:
        /******************** Functions from humidity sensor ********************/
